085028fbe1d35573f8b9895e9ea725ffc467d849
your name <taduyhoang10@gmail.com>
2023-02-24 15:15:38 +0700

    13th commit

---
+++ StartActivityDemo_week4/app/src/main/java/hcmute/edu/startactivitydemo_week4/MainActivity.java
@@ -0,0 +1,30 @@
+package hcmute.edu.startactivitydemo_week4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity2();
+            }
+        });
+    }
+
+    public void openActivity2() {
+        Intent intent = new Intent(this, Activity2.class);
+        startActivity(intent);
+    }
+}
