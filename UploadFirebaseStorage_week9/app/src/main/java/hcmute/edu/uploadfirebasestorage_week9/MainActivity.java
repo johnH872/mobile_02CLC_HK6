7cb03225692140fadac41a611905a8448909cb3c
your name <taduyhoang10@gmail.com>
2023-04-14 15:40:10 +0700

    41st commit

---
+++ UploadFirebaseStorage_week9/app/src/main/java/hcmute/edu/uploadfirebasestorage_week9/MainActivity.java
@@ -0,0 +1,141 @@
+package hcmute.edu.uploadfirebasestorage_week9;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.webkit.MimeTypeMap;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.MediaController;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.VideoView;
+
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+public class MainActivity extends AppCompatActivity {
+    VideoView videoView;
+    Button button;
+    ProgressBar progressBar;
+    EditText editText;
+    TextView txtVChooseVideo;
+    private Uri videoUri;
+    MediaController mediaController;
+    StorageReference storageReference;
+    DatabaseReference databaseReference;
+    Member member;
+    UploadTask uploadTask;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        member = new Member();
+        storageReference = FirebaseStorage.getInstance().getReference("Video");
+        databaseReference = FirebaseDatabase.getInstance().getReference("video");
+
+        videoView = findViewById(R.id.videoView_video);
+        button = findViewById(R.id.btn_upload_video);
+        progressBar = findViewById(R.id.progressBar);
+        editText = findViewById(R.id.EditText_name);
+        mediaController = new MediaController(this);
+        videoView.setMediaController(mediaController);
+        videoView.start();
+
+        txtVChooseVideo = findViewById(R.id.textView_ChooseVideo);
+        txtVChooseVideo.setOnClickListener(view -> {
+            chooseVideo();
+        });
+
+        button.setOnClickListener(view -> {
+            UploadVideo();
+        });
+
+    }
+
+    private void UploadVideo() {
+        String videoName = editText.getText().toString().trim();
+        String search = editText.getText().toString().toLowerCase();
+        if (videoUri != null || !TextUtils.isEmpty(videoName)) {
+            progressBar.setVisibility(View.VISIBLE);
+            final StorageReference reference = storageReference.child(System.currentTimeMillis() + "." + getExt(videoUri));
+            uploadTask = reference.putFile(videoUri);
+            Task<Uri> urlTask = uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+                @Override
+                public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                    if (!task.isSuccessful()) {
+                        throw task.getException();
+                    }
+                    return reference.getDownloadUrl();
+                }
+            })
+                    .addOnCompleteListener(new OnCompleteListener<Uri>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Uri> task) {
+                            if (task.isSuccessful()) {
+                                Uri downloadUri = task.getResult();
+                                progressBar.setVisibility(View.INVISIBLE);
+                                Toast.makeText(MainActivity.this, "Data saved", Toast.LENGTH_SHORT).show();
+                                member.setName(videoName);
+                                member.setVideoUrl(downloadUri.toString());
+                                member.setSearch(search);
+                                String i = databaseReference.push().getKey();
+                                databaseReference.child(i).setValue(member);
+                            } else {
+                                Toast.makeText(MainActivity.this, "failed", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        } else {
+            Toast.makeText(this, "All Field are required", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    ActivityResultLauncher getVideoActivityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if ( result.getResultCode() == RESULT_OK || result != null || result.getData() != null) {
+                        Intent data = result.getData();
+                        videoUri = data.getData();
+                        videoView.setVideoURI(videoUri);
+                    }
+                }
+            });
+
+    public void chooseVideo() {
+        Intent intent = new Intent();
+        intent.setType("video/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        getVideoActivityResultLauncher.launch(intent);
+    }
+
+    private String getExt(Uri uri) {
+        ContentResolver contentResolver = getContentResolver();
+        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+        return mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(uri));
+    }
+
+    public void showVideo(View view) {
+
+    }
+}
---
+++ UploadFirebaseStorage_week9/app/src/main/java/hcmute/edu/uploadfirebasestorage_week9/Member.java
@@ -0,0 +1,34 @@
+package hcmute.edu.uploadfirebasestorage_week9;
+
+public class Member {
+    private String name;
+    private String VideoUrl;
+    private String search;
+
+    public Member() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getVideoUrl() {
+        return VideoUrl;
+    }
+
+    public void setVideoUrl(String videoUrl) {
+        VideoUrl = videoUrl;
+    }
+
+    public String getSearch() {
+        return search;
+    }
+
+    public void setSearch(String search) {
+        this.search = search;
+    }
+}
